11c1df14d68a7197cfae14b1452202186eed91d5
codeisneverodd <codeisneverodd@gmail.com>
2023-03-22 04:21:56 +0900

    feat: add Link to Logo

--- src/lib/@components/Logo.tsx
+++ src/lib/@components/Logo.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-import { ComponentProps } from "react";
-import LogoImg from "public/profile-512x512.png";
 import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import LogoImg from "public/profile-512x512.png";
+import { ComponentProps } from "react";
 
 type LogoProps = {
   size?: string;
@@ -9,8 +10,10 @@ type LogoProps = {
 
 export default function Logo({ size = "120px", ...restProps }: LogoProps) {
   return (
-    <Box boxSize={size} position="relative" flexShrink="0">
-      <Image fill src={LogoImg} alt="로고" sizes={size} {...restProps} />
-    </Box>
+    <Link href="/">
+      <Box boxSize={size} position="relative" flexShrink="0">
+        <Image fill src={LogoImg} alt="로고" sizes={size} {...restProps} />
+      </Box>
+    </Link>
   );
 }
