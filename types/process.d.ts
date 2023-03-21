cfc50aafde83346212b729a65c41c501e5ded6c6
codeisneverodd <codeisneverodd@gmail.com>
2023-03-21 16:21:12 +0900

    feat: add env type

---
+++ types/process.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    GITHUB_PAT: string;
+  }
+}
